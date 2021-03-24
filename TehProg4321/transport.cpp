9ae01d5151843411a0bd3f536da822df28585e79
Георгий Лупоок <vip.lupook@mail.ru>
2021-03-24 21:18:37 +0700

    fix Class

--- TehProg4321/transport.cpp
+++ TehProg4321/transport.cpp
@@ -6,10 +6,6 @@
 transport* transport::read_transport(std::ifstream& stream) {
     int type;
     stream >> type;
-    if (type < 1 || type > 3) {
-        std::cout << "Input error." << std::endl;
-        return nullptr;
-    }
     transport* temp_t;
     switch (type) {
     case T_type::PLANES:
