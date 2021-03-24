932ce496ad5e8b3ffae22255ef1de57f72606c35
Георгий Лупоок <vip.lupook@mail.ru>
2021-03-24 20:05:38 +0700

    initial commit

---
+++ TehProg4321/planes.cpp
@@ -0,0 +1,15 @@
+#include "planes.h"
+
+bool planes::read(std::ifstream& stream) {
+    if (!stream.eof())
+        stream >> range;
+    else return false;
+    if (!stream.eof())
+        stream >> carry;
+    else return false;
+    return true;
+}
+
+void planes::out(std::ofstream& stream) {
+    stream << "Planes; Range: " << range << "; Carry: " << carry << ";" << std::endl;
+}
